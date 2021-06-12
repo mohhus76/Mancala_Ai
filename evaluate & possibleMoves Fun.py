013dc719fcaea7291f5d592401740e9d434a1e77
Mohammed Amr <40805337+gittatohubato@users.noreply.github.com>
2021-06-12 02:46:09 +0200

    adding extra_turn function

--- evaluate & possibleMoves Fun.py
+++ evaluate & possibleMoves Fun.py
@@ -9,3 +9,15 @@ def possibleMoves(state):
         if(state[i] != 0):
             possiblemoves.append(i-6)
     return possiblemoves
+
+
+def extra_turn(state, selected_hole, player_type):
+        num_of_stones_to_propagate = state[selected_hole - 1]
+        score_hole_index = 6
+        if player_type == "human":
+            score_hole_index = 13
+
+        if (((selected_hole - 1) + num_of_stones_to_propagate) % 14) == score_hole_index:  # last stone condition
+            return True
+
+        return False
