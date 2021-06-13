ce623ee686931b398f550ad8c30d5b7127eca240
Mohammed Emad <31746654+mohammedBadawi@users.noreply.github.com>
2021-06-13 02:48:15 +0200

    modifying the free turn condition

--- AI.py
+++ AI.py
@@ -3,58 +3,60 @@ from Utils import Utils
 
 class AI:
     @staticmethod
-    def alphaBetaSearch(state,level):    
+    def alphaBetaSearch(state,level,stealing):    
         alpha = -1000;
         beta = 1000;
-        maxV = AI.max_value(state,alpha,beta,level);
+        maxV = AI.max_value(state,alpha,beta,level,stealing);
         
         return maxV; #We need to return the child with this value
 
     @staticmethod
-    def max_value(state,alpha,beta,level):
+    def max_value(state,alpha,beta,level,stealing):
         v = -10000
         maxChild = state
-        
-        if(level == 0):
+        hole = 0
+        if(level == 0 or Utils.is_game_over(state)) :
             return Utils.evaluate(state),maxChild
     
-        for child in Utils.get_node_children(state,0,False):
-            #if(extraTurn(state,child_index)):
-                #v = max(v,AI.max_value(child,alpha,beta,level-1)[0])
+        for child in Utils.get_node_children(state,0,stealing):
+            if(Utils.extra_turn(state,child[1])):
+                v = max(v,AI.max_value(child[0],alpha,beta,level-1,stealing)[0])
                 
-           #else:
-            v = max(v,AI.min_value(child,alpha,beta,level-1)[0])
+            else:
+                v = max(v,AI.min_value(child[0],alpha,beta,level-1,stealing)[0])
             
             if(v>=beta): #cut off condition
-                return v,child
+                return v,child[0],child[1]
             
             if (v>alpha): #update alpha if the found score is bigger
                 alpha = v
-                maxChild = child
+                maxChild = child[0]
+                hole = child[1]
                 
-        return v,maxChild;
+        return v,maxChild,hole;
     @staticmethod
-    def min_value(state,alpha,beta,level):
+    def min_value(state,alpha,beta,level,stealing):
         v = 10000
         minChild = state
-        
-        if(level == 0):
+        hole = 0
+        if(level == 0 or Utils.is_game_over(state)):
             return Utils.evaluate(state),minChild
     
-        for child in Utils.get_node_children(state,1,False):
-            #if(extraTurn(state,child_index)):
-                #v = min(v,AI.min_value(child,alpha,beta,level-1)[0])
-            #else:
-            v = min(v,AI.max_value(child,alpha,beta,level-1)[0])
+        for child in Utils.get_node_children(state,1,stealing):
+            if(Utils.extra_turn(state,child[1])):
+                v = min(v,AI.min_value(child[0],alpha,beta,level-1,stealing)[0])
+            else:
+                v = min(v,AI.max_value(child[0],alpha,beta,level-1,stealing)[0])
             
             if(v<alpha): #cut off condition
-                return v,child
+                return v,child[0],child[1]
             
             if (v<beta): #update beta if the found score is smaller
                 beta = v
-                minChild = child
+                minChild = child[0]
+                hole = child[1]
                 
-        return v,minChild;
+        return v,minChild,hole;
 
 
 
